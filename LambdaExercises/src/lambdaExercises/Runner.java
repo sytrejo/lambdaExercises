87ed990ced0de3b2f3599152df9d905fcfc7317e
Silvia Trejo <s.y.trejo26@gmail.com>
2022-07-14 20:11:13 -0400

    Updating work and storing online.

---
+++ LambdaExercises/src/lambdaExercises/Runner.java
@@ -0,0 +1,20 @@
+package lambdaExercises;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		
+		
+		BankAccount account1 = new BankAccount(12345678, 987654, "Mr. J Smith", "Savings", 1.1, 25362.91);
+		BankAccount account2 = new BankAccount(87654321, 234567, "Ms. J Jones", "Current/Checking", 0.2, 550);
+		BankAccount account3 = new BankAccount(74639572, 946284, "Dr. T Williams", "Savings", 1.1, 4763.32);
+		BankAccount account4 = new BankAccount(94715453, 987654, "Ms. S Taylor", "Savings", 1.1, 10598.01);
+		BankAccount account5 = new BankAccount(16254385, 234567, "Mr. P Brown", "Current/Checking", 0.2, 25362.91);
+		BankAccount account6 = new BankAccount(12345678, 987654, "Mr. J Smith", "Savings", 1.1, 25362.91);
+		BankAccount account7 = new BankAccount(12345678, 987654, "Mr. J Smith", "Savings", 1.1, 25362.91);
+		BankAccount account8 = new BankAccount(12345678, 987654, "Mr. J Smith", "Savings", 1.1, 25362.91);
+		
+
+	}
+
+}
